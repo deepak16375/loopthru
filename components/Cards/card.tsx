df55a4497d1f90008f81d6504489c468831b60d5
Deepak <dk5007667@gmail.com>
2024-09-16 13:06:40 +0530

    error fixed

--- components/Cards/card.tsx
+++ components/Cards/card.tsx
@@ -1,5 +1,6 @@
 'use client'
 import React, { useState, useEffect } from 'react';
+import Image from 'next/image'; // Import Next.js Image component
 
 interface HoverCardProps {
   imageSrc: string;
@@ -8,32 +9,38 @@ interface HoverCardProps {
 }
 
 const HoverCard: React.FC<HoverCardProps> = ({ imageSrc, title, description }) => {
-    const [isHovered, setIsHovered] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
 
-    useEffect(() => {
-      if (isHovered) {
-        const audio = new Audio('/pop.mp3');
-        audio.play();
-      }
-    }, [isHovered]);
+  useEffect(() => {
+    if (isHovered) {
+      const audio = new Audio('/pop.mp3');
+      audio.play();
+    }
+  }, [isHovered]);
+
   return (
-    <div className="flex flex-col items-center"
+    <div
+      className="flex flex-col items-center"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
       <div className="relative w-72 md:w-96 h-auto rounded-lg overflow-hidden shadow-lg transition-transform transform hover:scale-105">
         <div className="w-full h-full">
-          <img
+          {/* Replace img with Image component from Next.js */}
+          <Image
             src={imageSrc}
             alt={title}
-            className="w-full h-full object-cover rounded-lg"
+            layout="responsive" // Ensures the image is responsive
+            width={400} // Example width
+            height={300} // Example height (change based on your image aspect ratio)
+            className="object-cover rounded-lg"
           />
         </div>
         {/* Overlay that will show on hover */}
         <div className="absolute inset-0 flex items-end justify-center opacity-0 hover:opacity-100 transition-opacity duration-300">
           <div className="w-full h-1/2 bg-gradient-to-t from-[#FFD53D] to-black opacity-95 flex items-center justify-center text-white ">
             <p className="text-center px-4 text-xs md:text-sm">{description}</p>
-          </div> 
+          </div>
         </div>
       </div>
       <h2 className="text-xl font-semibold p-10 text-center">{title}</h2>
